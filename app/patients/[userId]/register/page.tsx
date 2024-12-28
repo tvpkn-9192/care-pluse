8aac97c47764dd86beb1bdfe1bbcf56731c2848d
venkata pavan kumar Tirumalluri <vpktirumalluri@compreo.ai>
2024-12-28 23:36:01 +0530

    created registration page

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,16 +1,20 @@
-import PatientForm from "@/components/forms/PatientForm"
+import RegisterForm from "@/components/forms/RegisterForm"
+import { getUser } from "@/lib/actions/patient.actions"
 import Image from "next/image"
 import Link from "next/link"
+import React from "react"
 
-export default function Home() {
-  return (
-    <div className="flex h-screen max-h-screen">
+const Register = async({ params: { userId } }: SearchParamProps) => {
+    const user = await getUser(userId);
+
+    return(
+        <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image src={"/assets/icons/logo-full.svg"} alt={"patient"}
             height={1000} width={1000} className="mb-12 h-10 w-fit" />
 
-          <PatientForm />
+          <RegisterForm user={user}/>
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
@@ -21,8 +25,10 @@ export default function Home() {
         </div>
       </section>
 
-      <Image src={"/assets/images/onboarding-img.png"} alt={"patient"}
-            height={1000} width={1000} className="side-img max-w-[50%]"/>
+      <Image src={"/assets/images/register-img.png"} alt={"patient"}
+            height={1000} width={1000} className="side-img max-w-[390px]"/>
     </div>
-  )
+    )
 }
+
+export default Register
